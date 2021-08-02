c107bc3db7043b9c2e62d2c5c5e6119413eca8ab
p3lican <365mardigras@gmail.com>
2021-08-01 21:09:34 -0500

    sdfsdf

--- pages/documentary/index.vue
+++ pages/documentary/index.vue
@@ -5,7 +5,9 @@
       <div class="flex flex-col flex-wrap sm:flex-row">
       <nuxt-link  class=" mt-5 text-center p-2 sm:w-1/2 md:w-1/3" v-for="(project, index) in theProjects" :key="project.index" :to="project.path">
         <article>
-          <img :src="project.cover">
+          <picture class="h-60 block">
+            <img class="w-full h-full object-cover" :src="project.cover">
+          </picture>
           <h2 class="my-5">{{project.title}}</h2>
         </article>
       </nuxt-link>
